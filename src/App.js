f7938ca483db0f31bed6ffb66bc842bcc19c9e48
thanush96 <danushthanush@gmail.com>
2020-10-17 14:47:16 +0530

    Step 4 : Add Footer and Footer Container

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/layouts/Header';
 import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Navbar/>
+      <Header />
+      <Navbar />
+      <Footer/>
     </div>
   );
 }
