b6521fecce297890d69086c169fdc0b644f92006
thanush96 <danushthanush@gmail.com>
2020-10-17 13:31:36 +0530

    Step 3 : Add logo and Styles in Navigation bar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+import logo from '../../logo.jpg'
 
 const Navbar = () => {
     return (
-        <div>
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <a class="navbar-brand" href="#">Navbar</a>
+        <NavbarContainer>
+            <nav class="navbar navbar-expand-lg navbar-light" px-5 py-0>
+                <a class="navbar-brand" href="#">
+                    <img style={{width:"50px"}} src={logo} alt="logo"/>
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -20,8 +24,19 @@ const Navbar = () => {
                     </ul>
                 </div>
             </nav>
-        </div>
+        </NavbarContainer>
     );
 };
 
 export default Navbar;
+
+//MAIN NAVBAR CONTAINER
+const NavbarContainer = styled.div`
+    background:var(--dark-green);
+    .nav-link{
+        color:#fff !important;
+        &:hover{
+            background:var(--light-green);
+        }
+    }
+`;
