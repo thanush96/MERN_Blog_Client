61e77f5094d71196d5d62dbc21042bb0295fe5e3
thanush96 <danushthanush@gmail.com>
2020-10-19 20:26:06 +0530

    Step 10 :Send from value to MomgoDB with React Hooks

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -11,17 +11,17 @@ const Articles = ({ posts }) => {
                 !posts.length ? <img src={spinner} alt="loading.." />:
                 
                 posts.map((article, key) => (
-                <div className='container'>
+                <div className='container' key={key}>
                     <h2>{article.title}</h2>
                     <p>{article.article}</p>
                     <span className="badge badge-secondary p-2">{article.authorname}</span>
 
                     <div className="row my-5">
                         <div className="col-sm-2">
-                            <a href="" className="btn btn-outline-success"> Edit Article</a>
+                            <a to="/" className="btn btn-outline-success"> Edit Article</a>
                         </div>
                         <div className="col-sm-2">
-                            <a href="" className="btn btn-outline-danger"> Delete Article</a>
+                            <a to="/" className="btn btn-outline-danger"> Delete Article</a>
                         </div>
                     </div>
                 </div>
--- src/components/layouts/AddArticle.js
+++ src/components/layouts/AddArticle.js
@@ -1,30 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 export const AddArticle = () => {
+	const [title, setTitle] = useState('');
+	const [article, setArticle] = useState('');
+  const [authorname, setAuthorName] = useState('');
+  
+  const changeOnClick=e=>{
+    e.preventDefault();
+
+    const articles={
+      title,
+      article,
+      authorname 
+    };
+
+    axios
+    .post("/articles/add",articles)
+    .then(res=>console.log(res.data))
+    .catch(err=>{
+      console.log(err);
+    });
+
+
+  };
+
+
 	return (
 		<AddArticleContainer>
 			<div className="container">
 				<h1>Add New Artile</h1>
-				<form>
+				<form onSubmit={changeOnClick} encType="multypart/form-data" >
 					<div className="form-group">
 						<label htmlFor="authername">Author Name</label>
 						<br />
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Author Name" />
+						<input
+              type="text"
+              value={authorname}
+							onChange={(e) => setAuthorName(e.target.value)}
+							className="form-control"
+							placeholder="Author Name"
+						/>
 					</div>
 					<div className="form-group">
 						<label htmlFor="title">Title</label>
 						<br />
-						<input type="text" className="form-control" placeholder="Title" />
+						<input
+              type="text"
+              value={title}
+							onChange={(e) => setTitle(e.target.value)}
+							className="form-control"
+							placeholder="Title"
+						/>
 					</div>
 
 					<div className="form-group">
 						<label htmlFor="artile">Article</label>
 						<br />
-						<textarea className="form-control" rows="3"></textarea>
+						<textarea
+            value={article}
+							className="form-control"
+							onChange={(e) => setArticle(e.target.value)}
+							rows="3"
+						></textarea>
 					</div>
 
 					<button type="submit" className="btn btn-primary">
